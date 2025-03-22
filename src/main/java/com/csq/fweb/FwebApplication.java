0732532e3df04525a2c9c1f152dc828fd13e83ff
Davis <294639693@qq.com>
2025-03-23 01:35:47 +0800

    服务注册到consul 成功

--- src/main/java/com/csq/fweb/FwebApplication.java
+++ src/main/java/com/csq/fweb/FwebApplication.java
@@ -2,8 +2,13 @@ package com.csq.fweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class FwebApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/csq/fweb/config/SecurityConfig.java
+++ src/main/java/com/csq/fweb/config/SecurityConfig.java
@@ -3,26 +3,20 @@ package com.csq.fweb.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
-        requestHandler.setCsrfRequestAttributeName(null);
-
         http
-           .csrf(csrf -> csrf
-                   .csrfTokenRequestHandler(requestHandler)
-                   .ignoringRequestMatchers("/**")
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll()
             )
-           .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/**").permitAll()
-                        .anyRequest().authenticated()
-                );
+            .csrf(csrf -> csrf.disable());
         return http.build();
     }
 }
